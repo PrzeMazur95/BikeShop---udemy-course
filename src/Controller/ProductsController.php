ce479006094892103203e6053e54918ed581510a
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-04-12 20:00:51 +0200

    Add to basket with session handling

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -6,6 +6,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Repository\ProductRepository;
@@ -29,7 +31,7 @@ class ProductsController extends AbstractController
      * @Route("/products/{id}")
      */
 
-    public function details($id, ProductRepository $repo): Response
+    public function details($id, Request $request, ProductRepository $repo, SessionInterface $session): Response
     {
         $bike = $repo->find($id);
 
@@ -38,9 +40,22 @@ class ProductsController extends AbstractController
             throw $this->createNotFoundException('The product does not exist');
 
         }
+
+        //add to basket handling
+
+        $basket = $session->get('basket', []);
+
+        if ($request->isMethod('POST')){
+            $basket[$bike->getId()] = $bike;
+            $session->set('basket', $basket);
+        }
+
+        $IsInBasket = array_key_exists($bike->getId(), $basket);
+
+         
         return $this->render('details.html.twig',[
             'bike'=>$bike,
-            'inBasket' =>false
+            'inBasket' =>$IsInBasket 
         ]);
 
     }
